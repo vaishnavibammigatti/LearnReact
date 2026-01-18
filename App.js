98013c8907885a9a136ee8c00d694b844d556c9a
Vatsala <you@example.com>
2026-01-18 13:12:12 +0530

    Level 3

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return <h1 className="heading"> Namaste React</h1>;
+};
+
+//Component Composition
+const HeadingComponent = () => {
+  return (
+  <div>
+    <Title />
+    <h1 id="heading">Namaste React Functional Component</h1>;
+  </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Learn React
+# parcel 
+- Dev build
+- Local server
+- HMR - Hot Module Replacement
+- File watching algorithm
+- Caching - Faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Deifferential Bundling - support old browsers
+- Diagnostics
+- Error handling
+- HTTPs
+- Tree shaking - Remove unused code
+- Different dev and production builds
